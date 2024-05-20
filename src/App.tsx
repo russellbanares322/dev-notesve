741cb249f89f769b408c8de20a963826cb05e964
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-20 19:34:00 +0800

    feat: add container in parent wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Footer, Navbar } from "./components";
 
 function App() {
   return (
-    <>
+    <div className="p-10">
       <Navbar />
       Content
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,19 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="container flex items-center justify-between">
+        {/* Logo */}
+        <div>
+          <h1>Logo</h1>
+        </div>
+        {/* Nav Items */}
+        <ul className="flex items-center gap-3">
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
