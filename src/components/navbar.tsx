02b472654a4a941e240c236b63b60dbf7626da65
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-21 20:18:32 +0800

    feat: add logo in navbar's app title

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,15 @@
+import { FileCode2 } from "lucide-react";
+
 const Navbar = () => {
   return (
     <nav>
       <div className="container flex items-center justify-between">
         {/* Logo */}
         <div>
-          <h1>Logo</h1>
+          <h1 className="flex items-center gap-1 font-semibold">
+            <FileCode2 size={20} />
+            DevNotesve
+          </h1>
         </div>
         {/* Nav Items */}
         <ul className="flex items-center gap-3">
