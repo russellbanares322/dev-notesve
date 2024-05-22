1e827bded395bafe55c54318665930c14449e179
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-22 21:23:45 +0800

    feat: increase gap in login page elements

--- src/components/sign-in-button.tsx
+++ src/components/sign-in-button.tsx
@@ -5,10 +5,10 @@ import AppLogo from "./app-logo";
 
 const SignInButton = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full min-h-[95vh] gap-6">
+    <div className="flex flex-col items-center justify-center h-full min-h-[95vh] gap-7">
       <div className="flex flex-col items-center justify-center gap-2">
         <AppLogo />
-        <h1 className="text-3xl font-bold">Welcome back</h1>
+        <h1 className="text-3xl font-bold mt-7">Welcome back</h1>
         <p>Sign in to access your dashboard, settings and notes.</p>
       </div>
       <ClerkSignInButton mode="modal">
