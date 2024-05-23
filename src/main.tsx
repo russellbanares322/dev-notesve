5991a3f8922228fc29d67c03e0145f77a197de55
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-23 20:53:01 +0800

    feat: add redirection after user logs out

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <BrowserRouter>
           <App />
