6d3d81f6595a83029a4f059cc7c57fad350bca07
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-06-11 21:35:08 +0800

    feat: show menu icon in small screen device

--- src/components/layout/content-wrapper.tsx
+++ src/components/layout/content-wrapper.tsx
@@ -1,15 +1,22 @@
+import { MenuIcon } from "lucide-react";
 import Sidebar from "./sidebar";
+import { Button } from "../ui/button";
 
 type ContentWrapperProps = {
   children: React.ReactNode;
 };
 const ContentWrapper = ({ children }: ContentWrapperProps) => {
   return (
-    <div className="flex items-start h-full gap-1 mt-10 container">
+    <div className="flex items-start h-full gap-1 mt-10 container relative">
       <div className="hidden md:block">
         <Sidebar />
       </div>
-      <div>{children}</div>
+      <div className="absolute -top-6 left-2 md:hidden">
+        <Button size="sm">
+          <MenuIcon />
+        </Button>
+      </div>
+      <div className="pl-4 md:pl-0">{children}</div>
     </div>
   );
 };
