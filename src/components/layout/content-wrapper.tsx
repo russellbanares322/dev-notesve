aff3987f9c0df4b7b31dbadbedbc349a2db8e697
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-06-13 20:46:18 +0800

    feat: implement onClick functionality in menu bar

--- src/components/layout/content-wrapper.tsx
+++ src/components/layout/content-wrapper.tsx
@@ -1,22 +1,30 @@
-import { MenuIcon } from "lucide-react";
+import { MenuIcon, X } from "lucide-react";
 import Sidebar from "./sidebar";
 import { Button } from "../ui/button";
+import { useState } from "react";
 
 type ContentWrapperProps = {
   children: React.ReactNode;
 };
 const ContentWrapper = ({ children }: ContentWrapperProps) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdownVisibility = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className="flex items-start h-full gap-1 mt-10 container relative">
       <div className="hidden md:block">
         <Sidebar />
       </div>
       <div className="absolute -top-6 left-2 md:hidden">
-        <Button size="sm">
-          <MenuIcon />
+        <Button onClick={toggleDropdownVisibility} size="sm">
+          {!isDropdownOpen && <MenuIcon />}
+          {isDropdownOpen && <X />}
         </Button>
       </div>
-      <div className="pl-4 md:pl-0">{children}</div>
+      <div className="pl-4 pt-7 md:pl-0 md:pt-0">{children}</div>
     </div>
   );
 };
