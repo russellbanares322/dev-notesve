ec9633c68f7730c280f67ddbd44d057a7477503e
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-06-23 21:01:02 +0800

    feat: wrap app component in react query client

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -12,12 +13,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <BrowserRouter>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </BrowserRouter>
       </ThemeProvider>
     </ClerkProvider>
