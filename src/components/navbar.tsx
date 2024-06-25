2dfe75d1ab931560fd2d523e06da8a68c072bf22
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-06-25 21:02:07 +0800

    feat: instantiate getDevNotesByAuthorId api call

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,8 @@ import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
 import { UserButton } from "@clerk/clerk-react";
 import AppLogo from "./app-logo";
-import { useGetDevNotesByAuthorId } from "@/services/devnote/queries";
 
 const Navbar = () => {
-  const { data } = useGetDevNotesByAuthorId("a1");
-
-  console.log(data);
   return (
     <nav className="border-b py-3">
       <div className="container flex items-center justify-between">
--- src/services/axiosInstance/axiosInstance.ts
+++ src/services/axiosInstance/axiosInstance.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL =  import.meta.env.VITE_APP_BASE_URL
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_APP_BASE_URL,
+})
 
-export { axios as api }; 
+export { axiosInstance as api }
