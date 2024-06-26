f56ffa531254582a400ae5874d96fb2c6a790997
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-06-26 22:06:14 +0800

    feat: remove unused footer import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { SignedIn, SignedOut } from "@clerk/clerk-react";
-import { ContentWrapper, Footer, SignInButton } from "./components";
+import { ContentWrapper, SignInButton } from "./components";
 import { Route, Routes } from "react-router-dom";
 import { routes } from "./routes/routes";
 
