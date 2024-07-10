29ee7687ec12356530ccd0b28de5cbc80440076e
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-07-10 21:33:46 +0800

    feat: dynamically show close button when hovering devnote item

--- src/pages/devnote/DevNoteDetailsDisplay.tsx
+++ src/pages/devnote/DevNoteDetailsDisplay.tsx
@@ -1,15 +1,30 @@
 import { useDisplayDevNotesStore } from "@/store/useDisplayDevNotesStore";
 import { X } from "lucide-react";
+import { useState } from "react";
 
 const DevNoteDetailsDisplay = () => {
   const { selectedDevNotes } = useDisplayDevNotesStore();
+  const [hoveredItemId, setHoveredItemId] = useState<string | null>(null);
+
+  const onMouseEnter = (id: string) => {
+    setHoveredItemId(id);
+  };
+
+  const onMouseLeave = () => {
+    setHoveredItemId(null);
+  };
 
   return (
     <div className="flex items-center gap-2">
       {selectedDevNotes?.map((note) => (
-        <div className="flex items-center gap-1" key={note.devnote_id}>
+        <div
+          className="flex items-center gap-1 cursor-pointer"
+          onMouseEnter={() => onMouseEnter(note.devnote_id)}
+          onMouseLeave={onMouseLeave}
+          key={note.devnote_id}
+        >
           <p className="text-sm">{note.title}</p>
-          <X />
+          {hoveredItemId === note.devnote_id && <X />}
         </div>
       ))}
     </div>
