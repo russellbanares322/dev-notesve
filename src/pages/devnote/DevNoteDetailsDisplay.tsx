5c0f6b1eaefda8ee73a120adcd3a572878225663
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-07-12 20:50:09 +0800

    feat: remove onMouseEnter property when showing close icon in devnote details item

--- src/pages/devnote/DevNoteDetailsDisplay.tsx
+++ src/pages/devnote/DevNoteDetailsDisplay.tsx
@@ -1,36 +1,22 @@
 import { useDisplayDevNotesStore } from "@/store/useDisplayDevNotesStore";
 import { X } from "lucide-react";
-import { useState } from "react";
 
 const DevNoteDetailsDisplay = () => {
   const { selectedDevNotes, onRemoveDevNote } = useDisplayDevNotesStore();
-  const [hoveredItemId, setHoveredItemId] = useState<string | null>(null);
-
-  const onMouseEnter = (id: string) => {
-    setHoveredItemId(id);
-  };
-
-  const onMouseLeave = () => {
-    setHoveredItemId(null);
-  };
 
   return (
     <div className="flex items-center gap-2">
       {selectedDevNotes?.map((note) => (
         <div
-          className="cursor-pointer relative mr-3"
-          onMouseEnter={() => onMouseEnter(note.devnote_id)}
-          onMouseLeave={onMouseLeave}
+          className="cursor-pointer relative mr-3 group flex items-center gap-1"
           key={note.devnote_id}
         >
           <p className="text-sm">{note.title}</p>
-          {hoveredItemId === note.devnote_id && (
-            <X
-              size={17}
-              className="absolute top-0 -right-4"
-              onClick={() => onRemoveDevNote(note.devnote_id)}
-            />
-          )}
+          <X
+            size={17}
+            className="hidden group-hover:block"
+            onClick={() => onRemoveDevNote(note.devnote_id)}
+          />
         </div>
       ))}
     </div>
