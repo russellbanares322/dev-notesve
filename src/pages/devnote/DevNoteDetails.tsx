f08a8d0dba79a9595ef30449a9fb9cdc696ef959
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-07-21 19:37:43 +0800

    feat: display devnote category

--- src/pages/devnote/DevNoteDetails.tsx
+++ src/pages/devnote/DevNoteDetails.tsx
@@ -1,6 +1,8 @@
 import { useGetDevNote } from "@/services/devnote/queries";
 import { useParams } from "react-router-dom";
 import moment from "moment";
+import { Dot } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 const DevNoteDetails = () => {
   const { id } = useParams<{ id: string }>();
@@ -8,8 +10,14 @@ const DevNoteDetails = () => {
 
   return (
     <div className="mt-5">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-xl font-semibold">{data?.title?.toUpperCase()}</h1>
+      <div className="flex flex-col items-center justify-start">
+        <div className="flex items-center">
+          <h1 className="text-xl font-semibold">
+            {data?.title?.toUpperCase()}
+          </h1>
+          <Dot />
+          <Badge>{data?.category}</Badge>
+        </div>
         <span className="font-light text-xs">
           Date Created:{" "}
           <span className="font-medium">
