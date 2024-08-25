be80cd9258052dff9eecad849c9ab4cceaee1a53
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-08-25 20:19:14 +0800

    feat: instantiate sort direction filter select

--- src/pages/devnote/filterOptions/SortFilter.tsx
+++ src/pages/devnote/filterOptions/SortFilter.tsx
@@ -1,5 +1,30 @@
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const SortFilter = () => {
-  return <div>SortFilter</div>;
+  return (
+    <div>
+      <Label>Sort Direction</Label>
+      <Select value="1">
+        <SelectTrigger className="w-[180px]">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="0">Ascending</SelectItem>
+            <SelectItem value="1">Descending</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
 };
 
 export default SortFilter;
