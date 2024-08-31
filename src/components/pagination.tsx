1f1569cfdfabea297fd68d89f76d5a3d8c962fe3
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-08-31 23:39:08 +0800

    feat: include prev and next button in pagination

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,15 +1,22 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "./ui/button";
 
 const Pagination = () => {
   const customArray = new Array(5);
 
   return (
-    <div className="flex items-center gap-1">
+    <div className="flex items-center gap-1 justify-center mt-4">
+      <Button variant="outline">
+        <ChevronLeft />
+      </Button>
       {customArray.fill("").map((_, index) => (
-        <Button key={index} className="mt-4" variant="outline">
+        <Button key={index} variant="outline">
           {index + 1}
         </Button>
       ))}
+      <Button variant="outline">
+        <ChevronRight />
+      </Button>
     </div>
   );
 };
