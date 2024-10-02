ed5d03d28d939a3e5044fe4413b2317f9f2dcde7
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-10-02 21:03:08 +0800

    feat: instantiate page not found component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
+import PageNotFound from "./404/PageNotFound";
 import DevNoteCard from "./devnote/DevNoteCard";
 import DevNoteDetails from "./devnote/DevNoteDetails";
 import Home from "./home/Home";
 
-export {DevNoteCard,  DevNoteDetails, Home }
+export {DevNoteCard,  DevNoteDetails, Home, PageNotFound }
