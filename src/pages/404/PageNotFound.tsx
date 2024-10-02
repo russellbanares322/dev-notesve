59d6f25435ea7c5aec9ba197198d39b8abf94b03
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-10-02 22:02:38 +0800

    style: make page not found component responsive

--- src/pages/404/PageNotFound.tsx
+++ src/pages/404/PageNotFound.tsx
@@ -7,16 +7,22 @@ const PageNotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen h-full flex items-center justify-center gap-12">
-      <img className="max-h-96" src={pageNotFound} alt="Page not found" />
-      <div className="flex flex-col gap-3">
-        <p className="text-6xl font-semibold">Oops!</p>
-        <p className="text-lg font-medium text-gray-400 max-w-64 tracking-wide">
-          We couldn't find the page you're looking for
-        </p>
-        <Button className="w-fit" onClick={() => navigate("/")}>
-          <MoveLeft className="mr-2" /> Return to Home
-        </Button>
+    <div className="min-h-screen h-full flex items-center justify-center">
+      <div className="flex flex-wrap items-center justify-center gap-7 md:gap-12">
+        <img
+          className=" max-h-60 md:max-h-96"
+          src={pageNotFound}
+          alt="Page not found"
+        />
+        <div className="flex flex-col items-center md:items-start gap-5 md:gap-3">
+          <p className="text-4xl md:text-6xl font-semibold">Oops!</p>
+          <p className="text-base md:text-lg font-medium text-gray-400 max-w-full tracking-tight md:max-w-64 md:tracking-wide">
+            We couldn't find the page you're looking for
+          </p>
+          <Button className="w-fit" onClick={() => navigate("/")}>
+            <MoveLeft className="mr-2" /> Return to Home
+          </Button>
+        </div>
       </div>
     </div>
   );
