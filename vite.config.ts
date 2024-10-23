8d7df1a94558657d1650b0c0ecdea00ead0dae8a
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-10-23 19:45:09 +0800

    fix: resolve npm vulnerabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
-  plugins: [react()], build: {
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })], 
+  build: {
     rollupOptions: {
       output: {
         chunkFileNames: `[name].[hash].js`,
