0b59a5547cded26afaa5f9ae3677755175b71246
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-04 21:40:36 +0800

    chore: add return in onMakeFilterOptionSticky function

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -53,7 +53,7 @@ const Home = () => {
   };
 
   const onMakeFilterOptionSticky = () => {
-    setMakeFilterOptionSticky(window.scrollY > 130);
+    return setMakeFilterOptionSticky(window.scrollY > 130);
   };
 
   const onPageNumberChange = (selectedPageNumber: number) => {
